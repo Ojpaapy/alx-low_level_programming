55a2d708ce8cc65c24e1c7cfd4804cd31d427e27
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-16 17:20:13 +0100

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - using puts for printing
- *Return: 0 (Success)
- */
+*main - using puts for printing
+*Return: 0 (Success)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
