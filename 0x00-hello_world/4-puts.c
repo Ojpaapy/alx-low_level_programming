1b531858379a24772773a1867d95bf2280126ba1
Ojpaapy.com <avanrenrenj@gmail.com>
2023-06-16 09:23:52 -0700

    printing with printf

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-*main- using puts for printing
-*return: 0
+*main - using puts for printing
+*return: 0 (success)
 */
-
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle\n")
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - printing using the print f fucntion
+ * Return: 0 (sucess)
+ */
+
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art, \n");
+return (0);
+}
