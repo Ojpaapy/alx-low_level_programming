93ca9989ccdca02ab64358fcdaa629f7ff0d0a11
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 20:29:18 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,11 +15,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 lastnumb = n % 10;
 if (lastnumb > 5)
-printf("%d %d\n", n, lastnumb);
+printf("%d%d\n", n, lastnumb);
 if (lastnumb == 0)
-printf("%d %d\n", n, lastnumb);
+printf("%d%d\n", n, lastnumb);
 if (lastnumb < 6 && m != 0)
-printf("%d %d\n", n, lastnumb);
+printf("%d%d\n", n, lastnumb);
 return (0);
 }
 
