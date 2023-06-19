2d8ed6369f4739dd39e45bcf11f81c199b1fc7bc
Ojpaapy.com <avanrenrenj@gmail.com>
2023-06-19 13:24:45 -0700

    lower to uppercase convert

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - write lower & upper
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+int lowercase = 'a'
+int uppwercase = 'A'
+while (lowercase <= 'z')
+{
+putchar(lowercase);
+lowercase += 1;
+}
+while (uppercase <= 'Z'}
+{
+putchar(uppercase);
+uppercase += 1;
+}
+putchar('\n');
+return (0);
+}
