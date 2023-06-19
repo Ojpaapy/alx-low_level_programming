36174c9a8ab727086dc0c6211f4937caf9c7e56c
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 10:56:09 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int b;
 long int c;
 long long int d;
 float f;
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(a));
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(b));
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(c));
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(d));
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(f));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("size of a char: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
