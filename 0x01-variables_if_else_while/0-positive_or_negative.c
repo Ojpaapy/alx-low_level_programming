35a99a7d2f85d01da543c131463b6fccb4420264
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 15:47:35 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -12,11 +12,18 @@ int main(void)
 int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
+/* your code goes there*/
 if (n > 0)
+{
 printf("%d is positve\n", n);
+}
 else if (n == 0)
+{
 printf("%d is equal\n", n);
+}
 else
+{
 printf("%d is negative\n", n);
+}
 return (0);
 }
