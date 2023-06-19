cf7a7e29b1080a4d31e1bf970c8d6b3ab56d0e17
Ojpaapy.com <avanrenrenj@gmail.com>
2023-06-19 14:57:31 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - print reverse
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+char letter;
+for (letter = 'z'; letter >= 'a'; letter--)
+putchar(letter);
+putchar('\n');
+return (0);
+}
