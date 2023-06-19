cc7c658324390f54183ce3a118b28857fedebe47
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 10:55:35 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
-
 /**
  * main - -printing sizes available on cpu
  * Return: 0 (success)
  */
+
 int main(void)
 {
 char a;
 int b;
 long int c;
 long long int d;
-float e;
+float f;
 printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(a));
 printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(b));
 printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(c));
 printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(d));
-printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(e));
+printf("size of a char: %lu byte(s)\n", (unsigned long)Sizeof(f));
 return (0);
 }
