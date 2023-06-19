614ea6cc5f0fb3a0b8f3cca4a971696f298e15e8
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 11:55:24 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
 
 int main(void)
 {
-printf("size of a char: %c byte(s)\n", Sizeof(char));
-printf("size of a int: %d byte(s)\n", Sizeof(int));
-printf("size of a long int: %ld byte(s)\n", Sizeof(long int));
-printf("size of a long long int: %lld byte(s)\n", Sizeof(long long int));
-printf("size of a float: %f byte(s)", Sizeof(float));
+printf("size of a char: %c byte(s)\n", sizeof(char));
+printf("size of a int: %d byte(s)\n", sizeof(int));
+printf("size of a long int: %ld byte(s)\n", sizeof(long int));
+printf("size of a long long int: %lld byte(s)\n", sizeof(long long int));
+printf("size of a float: %f byte(s)", sizeof(float));
 return (0);
 }
