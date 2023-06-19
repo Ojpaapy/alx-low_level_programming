63636e1e9f2fda83402ba93c14e9530a2252703c
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-19 15:57:38 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,16 +8,16 @@
 int main(void)
 {
 int n;
-int m;
+int lastnumb;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-m = n % 10;
-if (m > 5)
-printf("Last digits of %d is %d and its greater than 5\n", n, m);
-if (m == 0)
-printf("Last digits of %d is %d and its greater than 0\n", n, m);
-if (m < 6 && m != 0)
-printf("Last digits of %d is %d and its less than 6 and not 0\n", n, m);
+lastnumb = n % 10;
+if (lastnumb > 5)
+printf("Last digits of %d is %d and its greater than 5\n", n, lastnumb);
+if (lastnumb == 0)
+printf("Last digits of %d is %d and its greater than 0\n", n, lastnumb);
+if (lastnumb < 6 && m != 0)
+printf("Last digits of %d is %d and its less than 6 and not 0\n", n, lastnumb);
 return (0);
 }
 
