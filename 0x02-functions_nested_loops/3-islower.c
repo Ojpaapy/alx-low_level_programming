db27a2800722cbc8f6259e1b3b027da60cdc4089
Ojpaapy.com <avanrenrenj@gmail.com>
2023-06-21 07:05:50 -0700

    getting sign

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,10 @@
 #include "main.h"
+
 /**
-<<<<<<< HEAD
- * _is_lower - checks code to see if its lowercase
-=======
  * _islower-Checks code to see if its lowercase
->>>>>>> 8ce4d22b338110ec79bbc6ddfefa080ddf55678b
  * Return: 1 for lowercase character or 0 for anything else
  */
+
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
