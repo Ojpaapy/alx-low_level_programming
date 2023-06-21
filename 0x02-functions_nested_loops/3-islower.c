4752012a03e55c24cc21eb29b5e8915600a49c48
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-21 14:21:44 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,6 @@
 #include "main.h"
-
 /**
- * _islower - Checks code to see if its lowercase
+ * main - Checks code to see if its lowercase
  * Return: 1 for lowercase character or 0 for anything else
  */
 int _islower(int c)
