b5ab509481b81b27fcd77a755bfe87f67995e805
Ojpaapy.com <avanrenrenj@gmail.com>
2023-06-21 06:18:43 -0700

    Lowercase

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,18 @@
 #include "main.h"
 /**
- * main - print_alphabet_x10
+ * print_alphabet_x10 - printing alphabet ten times
  * Return: void
  */
 void print_alphabet_x10(void)
 {
 char c;
-int i = 0;
-while (i <= 0)
+int i;
+for (i = 0; i < 10; i++)
 {
 for (c = 'a'; c <= 'z'; c++)
 {
 _putchar(c);
 }
 _putchar('\n');
-i++;
 }
 }
