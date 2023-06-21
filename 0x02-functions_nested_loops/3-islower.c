ca541cbbb455c79326e9dc7d85bcc6bf7f512cba
Ojpaapy <135063027+Ojpaapy@users.noreply.github.com>
2023-06-21 15:09:20 +0100

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
- * _islower - Checks code to see if its lowercase
+ * _islower - checks code to see if its lowercase
  * @c: The number to be checked 
  * Return: 1 for lowercase character or 0 for anything else
  */
-
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
